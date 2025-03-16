e791b2a525c7fd0416a954e8cc0680c2d23f57eb
miguel <matheusemiguelozzy@gmail.com>
2025-03-15 21:24:35 -0300

    Adicionando arquivos do Aplicativo ao repositorio remoto

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncarregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboclasse = new System.Windows.Forms.ComboBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.btninserir = new System.Windows.Forms.Button();
+            this.btnalterar = new System.Windows.Forms.Button();
+            this.btnpesquisar = new System.Windows.Forms.Button();
+            this.btnexcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btncarregar
+            // 
+            this.btncarregar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncarregar.Location = new System.Drawing.Point(673, 154);
+            this.btncarregar.Name = "btncarregar";
+            this.btncarregar.Size = new System.Drawing.Size(135, 45);
+            this.btncarregar.TabIndex = 0;
+            this.btncarregar.Text = "Carregar dados";
+            this.btncarregar.UseVisualStyleBackColor = true;
+            this.btncarregar.Click += new System.EventHandler(this.btncarregar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(586, 336);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Classe:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(334, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(334, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Email:";
+            // 
+            // cboclasse
+            // 
+            this.cboclasse.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboclasse.FormattingEnabled = true;
+            this.cboclasse.Items.AddRange(new object[] {
+            "BSI1",
+            "BSI2",
+            "BSI3",
+            "BSI4",
+            "ENG1",
+            "ENG2",
+            "ENG3",
+            "ENG4",
+            "PORT1",
+            "PORT2",
+            "TURB1",
+            "TURB2",
+            "MEDIC1",
+            "MEDIC2"});
+            this.cboclasse.Location = new System.Drawing.Point(153, 97);
+            this.cboclasse.Name = "cboclasse";
+            this.cboclasse.Size = new System.Drawing.Size(139, 26);
+            this.cboclasse.TabIndex = 6;
+            this.cboclasse.Text = "selecione...";
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(128, 36);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(164, 26);
+            this.txtid.TabIndex = 7;
+            // 
+            // txtnome
+            // 
+            this.txtnome.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnome.Location = new System.Drawing.Point(385, 37);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(187, 26);
+            this.txtnome.TabIndex = 8;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(385, 98);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(187, 26);
+            this.txtemail.TabIndex = 9;
+            // 
+            // btninserir
+            // 
+            this.btninserir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninserir.Location = new System.Drawing.Point(673, 220);
+            this.btninserir.Name = "btninserir";
+            this.btninserir.Size = new System.Drawing.Size(135, 45);
+            this.btninserir.TabIndex = 10;
+            this.btninserir.Text = "Inserir Dados";
+            this.btninserir.UseVisualStyleBackColor = true;
+            this.btninserir.Click += new System.EventHandler(this.btninserir_Click);
+            // 
+            // btnalterar
+            // 
+            this.btnalterar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnalterar.Location = new System.Drawing.Point(673, 281);
+            this.btnalterar.Name = "btnalterar";
+            this.btnalterar.Size = new System.Drawing.Size(135, 45);
+            this.btnalterar.TabIndex = 11;
+            this.btnalterar.Text = "Alterar Dados";
+            this.btnalterar.UseVisualStyleBackColor = true;
+            this.btnalterar.Click += new System.EventHandler(this.btnalterar_Click);
+            // 
+            // btnpesquisar
+            // 
+            this.btnpesquisar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpesquisar.Location = new System.Drawing.Point(673, 345);
+            this.btnpesquisar.Name = "btnpesquisar";
+            this.btnpesquisar.Size = new System.Drawing.Size(135, 45);
+            this.btnpesquisar.TabIndex = 12;
+            this.btnpesquisar.Text = "Pesquisar Dados";
+            this.btnpesquisar.UseVisualStyleBackColor = true;
+            this.btnpesquisar.Click += new System.EventHandler(this.btnpesquisar_Click);
+            // 
+            // btnexcluir
+            // 
+            this.btnexcluir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexcluir.Location = new System.Drawing.Point(673, 408);
+            this.btnexcluir.Name = "btnexcluir";
+            this.btnexcluir.Size = new System.Drawing.Size(135, 45);
+            this.btnexcluir.TabIndex = 13;
+            this.btnexcluir.Text = "Excluir Dados";
+            this.btnexcluir.UseVisualStyleBackColor = true;
+            this.btnexcluir.Click += new System.EventHandler(this.btnexcluir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(855, 507);
+            this.Controls.Add(this.btnexcluir);
+            this.Controls.Add(this.btnpesquisar);
+            this.Controls.Add(this.btnalterar);
+            this.Controls.Add(this.btninserir);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.cboclasse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btncarregar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agenda";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncarregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboclasse;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Button btninserir;
+        private System.Windows.Forms.Button btnalterar;
+        private System.Windows.Forms.Button btnpesquisar;
+        private System.Windows.Forms.Button btnexcluir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data; // IMPORTA A CONEXAO COM O BANCO DE DADOS
+using MySql.Data.MySqlClient; //// IMPORTA A CONEXAO COM O BANCO DE DADOS (tem que colocar esses 2 sempre para conectar com o banco de dados
+
+namespace agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string MysqlClientString = "Server = localhost; Port=3306;" + "User Id = root; Database =bdagenda; SSL Mode= 0"; //Indica o caminho para o banco de dados que a gente quer usar
+
+        private void btncarregar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(MysqlClientString);
+            conn.Open();
+            DataTable dt = new DataTable(); //Cria o local para os registros
+            MySqlDataAdapter da = new MySqlDataAdapter // É o mecanismo que vai pegando registro por registro e colocar no dentro do dt (dataTable)
+            ("SELECT * from tbalunos", conn); // Vai carregar o da, o da (DataApter executa essa linha
+            da.Fill(dt); //Pega os registros e coloca no datatable
+            dataGridView1.DataSource = dt; //Exibe tudo que está no DataTable ir dataGridView
+            dataGridView1.Columns[0].Width = 100;
+            dataGridView1.Columns[1].Width = 200;
+            dataGridView1.Columns[2].Width = 100;  //Muda o tamanho da coluna dentro do dataGridView
+            dataGridView1.Columns[3].Width = 300;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btninserir_Click(object sender, EventArgs e)
+        {
+            // Insere os dados nos campos de texto
+            string sql = "insert into tbalunos(ID,nome,classe,email) " +
+           "values('" + txtid.Text + "', '" + txtnome.Text + "','" + cboclasse.Text + "','" + txtemail.Text + "')";
+            MySqlConnection conn = new MySqlConnection(MysqlClientString); // Cria a conexao
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(); //Cria uma variavel para executar comandos SQL
+            cmd.Connection = conn;
+            cmd.CommandText = sql; // Faz a conexao com com o banco de dados
+            cmd.ExecuteNonQuery(); // Ele executa o cmd
+            MessageBox.Show("Gravado com sucesso");
+            btncarregar_Click(sender, e); // Ele vai executar a ação do button1 no click do button 2 tambem
+            txtid.Clear();
+            txtnome.Clear();
+            txtemail.Clear();
+            cboclasse.Text = "";
+            // Esses 4 limpam a caixa de texto
+        }
+
+        private void btnpesquisar_Click(object sender, EventArgs e)
+        {
+            try //Para Caso o usuario digite um ID que não existe ele vai dar um aviso
+            {
+                MySqlConnection conn = new MySqlConnection(MysqlClientString);
+                conn.Open();
+                DataTable dt = new DataTable(); 
+                MySqlDataAdapter da = new MySqlDataAdapter 
+                ("SELECT * from tbalunos where id = '" +txtid.Text+"'", conn); // where para ele pegar só do ID
+                da.Fill(dt);
+                txtnome.Text = dt.Rows[0].Field<string>("nome"); 
+                txtemail.Text = dt.Rows[0].Field<string>("email"); // Irá mostrar nas caixas de texto após o usario escolher o ID.
+                cboclasse.Text = dt.Rows[0].Field<string>("classe"); // Não irá mostrar no datagridview
+                dataGridView1.DataSource = dt;
+            }
+            catch // Para Caso o usuario digite um ID que não existe ele vai dar um aviso
+            {
+                MessageBox.Show("Aluno não cadastrado","Alerta",
+                MessageBoxButtons.OK,MessageBoxIcon.Information); // Deixa a caixa de alerta mais bonita.
+            }
+        }
+
+        private void btnalterar_Click(object sender, EventArgs e)
+        {
+            string sql = "update tbalunos set nome = '" + txtnome.Text + "', classe = '" + cboclasse.Text + "', email = '" + txtemail.Text + "' where id = '"+txtid.Text+"'"; // Altera os dados pelas caixas de texto
+            MySqlConnection conn = new MySqlConnection(MysqlClientString); 
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(); 
+            cmd.Connection = conn;
+            cmd.CommandText = sql; 
+            cmd.ExecuteNonQuery(); 
+            MessageBox.Show("Alterado com sucesso");
+            btncarregar_Click(sender, e); 
+            txtid.Clear();
+            txtnome.Clear();
+            txtemail.Clear();
+            cboclasse.Text = "";
+        }
+
+        private void btnexcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
